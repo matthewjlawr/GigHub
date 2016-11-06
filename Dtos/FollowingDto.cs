67540e1fc6cc01188828ee32613d14e2d99c0268
Matthew <matthew@matthewlawrence.net>
2016-11-06 15:02:46 -0600

    Implement view gigs i'm attending

---
+++ Dtos/FollowingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GigHub.Dtos
+{
+    public class FollowingDto
+    {
+        public int FollowingId { get; set; }
+        public string FolloweeId { get; set; }
+        public string FollowerId { get; set; }
+    }
+}
